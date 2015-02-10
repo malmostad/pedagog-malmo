9eec92930dfa5caf3fa07791595b5da707a4aba2
Olof Sellgren <olof@goodold.se>
2015-02-10 09:09:46 +0100

    Redesign front page articles

--- partials/loop-articles-start.php
+++ partials/loop-articles-start.php
@@ -1,4 +1,4 @@
-<?php $articles_loop = new WP_Query( array('post_type' => 'artiklar', 'posts_per_page' => 3, )); ?>
+<?php $articles_loop = new WP_Query( array('post_type' => 'artiklar', 'posts_per_page' => 4, )); ?>
 <div class="container">
 
     <?php if ( $articles_loop->have_posts() ) : ?>
@@ -21,7 +21,7 @@
             <p><a href="<?php echo get_permalink(); ?>"><?php the_excerpt(); ?></a></p>
             <div class="meta">
               <span class="article-date"><?php go_smartdate(); ?></span>
-              <span class="categories bp-medium-up"> • 
+              <span class="categories bp-medium-up"> •
                 <?php echo get_the_term_list( $articles_loop->post->ID, 'artikelkategorier', '', ', ','' ); ?>
               </span>
             </div>
@@ -32,15 +32,19 @@
       <?php $first_article = false; ?>
       <div class="row front-article-small-wrapper"><!-- row 2 -->
     <?php else: ?>
-      <article class="col-sm-6 front-article-small body-copy">
-        <div class="post-label">Artikel</div>
-        <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <p><a href="<?php echo get_permalink(); ?>"><?php the_excerpt(); ?></a></p>
-        <div class="meta">
-          <span class="article-date"><?php go_smartdate(); ?></span>
-          <span class="categories bp-medium-up"> • 
-            <?php echo get_the_term_list( $articles_loop->post->ID, 'artikelkategorier', '', ', ','' ); ?>
-          </span>
+      <article class="col-sm-4 front-article-small">
+        <?php if ( has_post_thumbnail() ): //TODO: FALLBACK IF NO IMAGE?>
+          <a href="<?php echo get_permalink() ?>"><?php the_post_thumbnail('loop-image-large') ?></a>
+        <?php endif; ?>
+        <div class="body-copy content">
+          <div class="post-label">Artikel</div>
+          <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <div class="meta">
+            <span class="article-date"><?php go_smartdate(); ?></span>
+            <!-- <span class="categories bp-medium-up"> •
+              <?php echo get_the_term_list( $articles_loop->post->ID, 'artikelkategorier', '', ', ','' ); ?>
+            </span> -->
+          </div>
         </div>
       </article>
     <?php endif; ?>
