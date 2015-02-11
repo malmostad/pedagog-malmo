4f6563080745c6473ae682d31b9adc4103e335a2
Olof Sellgren <olof@goodold.se>
2015-02-11 12:03:00 +0100

    Add home link in sticky header

--- partials/sticky-header.php
+++ partials/sticky-header.php
@@ -1,6 +1,8 @@
 <div id="sticky-header" class="sticky-header-wrapper sticky-header--hidden">
   <div class="sticky-header">
-    <img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Pedagog Malmö">
+    <a href="<?php echo home_url(); ?>">
+      <img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Pedagog Malmö">
+    </a>
     <nav class="main-nav" role="navigation">
       <?php pedagog_nav(); ?>
     </nav>
