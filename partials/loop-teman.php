b137d6bca645a2ae2101715082029653f3199f74
Olof Sellgren <olof@goodold.se>
2015-09-28 19:48:17 +0200

    Remove featured image from the ”Teman” loop

--- partials/loop-teman.php
+++ partials/loop-teman.php
@@ -1,11 +1,5 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
   <article id="post-<?php the_ID(); ?>" <?php post_class('loop-item loop-item--tema'); ?>>
-    <?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-        <?php echo get_the_label(); ?>
-        <?php the_post_thumbnail('loop-image-large'); ?>
-      </a>
-    <?php endif; ?>
       <?php $tema_image = get_field('tema_image'); ?>
       <div class="tema-image">
         <img src="<?php echo $tema_image['sizes']["tema-image-large"]; ?>" alt="">
